b88efb2acfb8ef734a65acb74bcac63560f26a3e
itsWindows11 <itswin11@outlook.com>
2023-12-14 19:34:32 +0200

    Added sample

---
+++ src/NtfsReader.Sample/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.IO.Filesystem.Ntfs;
+
+var ntfsReader = new NtfsReader(new DriveInfo("C:\\"), RetrieveMode.Minimal);
+
+var nodes = ntfsReader.GetNodes("C:\\");
+
+foreach (INode node in nodes)
+{
+    Console.WriteLine(node.FullName);
+}
